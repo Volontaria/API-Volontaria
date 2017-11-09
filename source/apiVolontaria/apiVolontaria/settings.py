1bc1d4be9408208834e9b95cc053f273ced8655a
RignonNoel <rignon.noel@gmail.com>
2017-11-09 17:32:55 -0500

    add standard pagination of DRF

--- source/apiVolontaria/apiVolontaria/settings.py
+++ source/apiVolontaria/apiVolontaria/settings.py
@@ -146,6 +146,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework.filters.DjangoFilterBackend',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.'
+                                'LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 
