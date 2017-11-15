bb640449fd7ba5332848a63ba3f291307b537690
RignonNoel <rignon.noel@gmail.com>
2017-11-14 19:49:59 -0500

    add cycle model

--- source/apiVolontaria/volunteer/models.py
+++ source/apiVolontaria/volunteer/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Cycle(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Cycles'
+
+    name = models.CharField(
+        verbose_name="Name",
+        max_length=100,
+    )
+
+    start_date = models.DateTimeField(
+        verbose_name="Start date",
+    )
+
+    end_date = models.DateTimeField(
+        verbose_name="End date",
+    )
