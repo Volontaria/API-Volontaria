7f19987474ff3c0bb562defb355ee45b6d701328
RignonNoel <rignon.noel@gmail.com>
2017-12-05 16:45:23 -0500

    add ActivationToken model

--- source/apiVolontaria/apiVolontaria/models.py
+++ source/apiVolontaria/apiVolontaria/models.py
@@ -4,6 +4,62 @@ from django.db import models
 from django.utils import timezone
 from rest_framework.authtoken.models import Token
 from django.conf import settings
+from django.contrib.auth.models import User
+import binascii
+import os
+
+
+class ActivationToken(models.Model):
+    """Class of Token to allow User to activate his account."""
+
+    key = models.CharField(
+        verbose_name="Key",
+        max_length=40,
+        primary_key=True
+    )
+
+    user = models.OneToOneField(
+        User,
+        related_name='activation_token',
+        on_delete=models.CASCADE,
+        verbose_name="User"
+    )
+
+    created = models.DateTimeField(
+        verbose_name="Creation date",
+        auto_now_add=True
+    )
+
+    expires = models.DateTimeField(
+        verbose_name="Expiration date",
+        blank=True,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+            self.expires = timezone.now() + timezone.timedelta(
+                minutes=settings.ACTIVATION_TOKENS['MINUTES']
+            )
+        return super(ActivationToken, self).save(*args, **kwargs)
+
+    @staticmethod
+    def generate_key():
+        """Generate a new key"""
+        return binascii.hexlify(os.urandom(20)).decode()
+
+    @property
+    def expired(self):
+        """Returns a boolean indicating token expiration."""
+        return self.expires <= timezone.now()
+
+    def expire(self):
+        """Expires a token by setting its expiration date to now."""
+        self.expires = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.key
 
 
 class TemporaryToken(Token):
