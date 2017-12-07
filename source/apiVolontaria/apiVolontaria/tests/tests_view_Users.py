07ac87ad2b50a9a6c6954e0907d23c42ecb20e0c
RignonNoel <rignon.noel@gmail.com>
2017-12-07 13:24:11 -0500

    add listing of users in Users view

--- source/apiVolontaria/apiVolontaria/tests/tests_view_Users.py
+++ source/apiVolontaria/apiVolontaria/tests/tests_view_Users.py
@@ -7,12 +7,21 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 from ..models import ActivationToken
+from ..factories import UserFactory, AdminFactory
 
 
 class UsersTests(APITestCase):
 
     def setUp(self):
         self.client = APIClient()
+
+        self.user = UserFactory()
+        self.user.set_password('Test123!')
+        self.user.save()
+
+        self.admin = AdminFactory()
+        self.admin.set_password('Test123!')
+        self.admin.save()
 
     def test_create_new_user(self):
         """
@@ -96,3 +105,60 @@ class UsersTests(APITestCase):
 
         content = {"password": ["This field is required."]}
         self.assertEqual(json.loads(response.content), content)
+
+    def test_list_users(self):
+        """
+        Ensure we can list all users.
+        """
+        self.client.force_authenticate(user=self.admin)
+
+        response = self.client.get(reverse('users'))
+
+        self.assertEqual(json.loads(response.content)['count'], 2)
+
+        first_user = json.loads(response.content)['results'][0]
+        self.assertEqual(first_user['id'], self.user.id)
+
+        # Check the system doesn't return attributes not expected
+        attributes = ['id', 'username', 'email', 'first_name',
+                      'last_name', 'is_active']
+        for key in first_user.keys():
+            self.assertTrue(
+                key in attributes,
+                'Attribute "{0}" is not expected but is '
+                'returned by the system.'.format(key)
+            )
+            attributes.remove(key)
+
+        # Ensure the system returns all expected attributes
+        self.assertTrue(
+            len(attributes) == 0,
+            'The system failed to return some '
+            'attributes : {0}'.format(attributes)
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_list_users_without_authenticate(self):
+        """
+        Ensure we can't list users without authentication.
+        """
+        response = self.client.get(reverse('users'))
+
+        content = {"detail": "Authentication credentials were not provided."}
+        self.assertEqual(json.loads(response.content), content)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_list_users_without_permissions(self):
+        """
+        Ensure we can't list users without permissions.
+        """
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.get(reverse('users'))
+
+        content = {"detail": "You are not authorized to list users."}
+        self.assertEqual(json.loads(response.content), content)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
