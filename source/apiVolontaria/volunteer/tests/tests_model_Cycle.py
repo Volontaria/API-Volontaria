d28196173cfcdc44f91259255a482006ff5e127c
RignonNoel <rignon.noel@gmail.com>
2017-12-23 21:34:18 -0500

    add test on is_active property on cycle

--- source/apiVolontaria/volunteer/tests/tests_model_Cycle.py
+++ source/apiVolontaria/volunteer/tests/tests_model_Cycle.py
@@ -18,3 +18,35 @@ class CycleTests(APITestCase):
         )
 
         self.assertEquals(token.name, 'my cycle')
+
+    def test_is_active_property_true(self):
+        """
+        Ensure we have True if the cycle is active
+        """
+        start_date = timezone.now()
+        end_date = start_date + timezone.timedelta(
+            minutes=100,
+        )
+
+        cycle = Cycle.objects.create(
+            name='my cycle',
+            start_date=start_date,
+            end_date=end_date,
+        )
+
+        self.assertEquals(cycle.is_active, True)
+
+    def test_is_active_property_false(self):
+        """
+        Ensure we have False if the cycle is not active
+        """
+        start_date = timezone.now()
+        end_date = start_date
+
+        cycle = Cycle.objects.create(
+            name='my cycle',
+            start_date=start_date,
+            end_date=end_date,
+        )
+
+        self.assertEquals(cycle.is_active, False)
