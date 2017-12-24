2afd71f289cbce00c8f59289de485f73b29b0a0c
RignonNoel <rignon.noel@gmail.com>
2017-12-23 21:34:01 -0500

    add new model event

--- source/apiVolontaria/volunteer/admin.py
+++ source/apiVolontaria/volunteer/admin.py
@@ -5,3 +5,4 @@ from . import models
 admin.site.register(models.Cycle)
 admin.site.register(models.TaskType)
 admin.site.register(models.Cell)
+admin.site.register(models.Event)
--- source/apiVolontaria/volunteer/models.py
+++ source/apiVolontaria/volunteer/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models, IntegrityError
+from django.utils import timezone
+
 
 from location.models import Address
 
@@ -24,6 +27,13 @@ class Cycle(models.Model):
         blank=True,
         null=True
     )
+
+    @property
+    def is_active(self):
+        now = timezone.now()
+        if self.end_date > now > self.start_date:
+            return True
+        return False
 
 
 class TaskType(models.Model):
@@ -76,3 +86,111 @@ class Cell(models.Model):
 
     def __str__(self):
         return '{}, {}'.format(self.name, str(self.address))
+
+
+class Event(models.Model):
+    """
+
+    This class represents the Event model.
+
+    """
+    class Meta:
+        verbose_name_plural = 'Events'
+
+    start_date = models.DateTimeField(
+        verbose_name="Begin date",
+        blank=False,
+        null=False,
+    )
+
+    end_date = models.DateTimeField(
+        verbose_name="End date",
+        blank=False,
+        null=False,
+    )
+
+    nb_volunteers_needed = models.PositiveIntegerField(
+        verbose_name="Number of volunteers (needed)",
+        default=0,
+    )
+    nb_volunteers_standby_needed = models.PositiveIntegerField(
+        verbose_name="Number of volunteers on hold (needed)",
+        default=0,
+    )
+
+    volunteers = models.ManyToManyField(
+        User,
+        verbose_name="Volunteers",
+        related_name="events",
+        blank=True,
+    )
+
+    volunteers_standby = models.ManyToManyField(
+        User,
+        verbose_name="Volunteers on hold",
+        related_name="events_standby",
+        blank=True,
+    )
+
+    cell = models.ForeignKey(
+        Cell,
+        verbose_name="Cell",
+        blank=False,
+    )
+
+    cycle = models.ForeignKey(
+        Cycle,
+        verbose_name="Cycle",
+        blank=False,
+    )
+
+    task_type = models.ForeignKey(
+        TaskType,
+        verbose_name="Task type",
+        blank=False,
+    )
+
+    def clean(self):
+        if self.start_date and self.end_date:
+
+            if self.start_date > self.end_date:
+                error = "The start date needs to be older than end date."
+                raise IntegrityError(error)
+
+            cycle = None
+            try:
+                cycle = self.cycle
+            except Exception:
+                pass
+
+            if cycle:
+                if cycle.start_date and cycle.start_date > self.start_date:
+                    error = "The start date can't be older than " \
+                            "start date of the cycle."
+                    raise IntegrityError(error)
+
+                if cycle.end_date and cycle.end_date < self.end_date:
+                    error = "The end date can't be younger than " \
+                            "end date of the cycle."
+                    raise IntegrityError(error)
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(Event, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return '{0}, {1}, {2}, {3} - {4}'.format(
+            self.cell,
+            self.cycle,
+            self.tasktype,
+            str(self.start_date),
+            str(self.end_date),
+        )
+
+    @property
+    def is_expired(self):
+        return self.end_date <= timezone.now()
+
+    @property
+    def is_active(self):
+        return self.cycle.is_active
