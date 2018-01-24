7f7f619d426d17f87416654280e6016457d81237
RignonNoel <rignon.noel@gmail.com>
2018-01-23 19:45:32 -0500

    upgrade of participation's admin panel

--- source/apiVolontaria/volunteer/admin.py
+++ source/apiVolontaria/volunteer/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 
 from . import models
 
+
+class ParticipationAdmin(admin.ModelAdmin):
+    search_fields = ['user__username']
+    list_display = ['user', 'standby', 'start_date', 'end_date', 'cell']
+
+
 admin.site.register(models.Cycle)
 admin.site.register(models.TaskType)
 admin.site.register(models.Cell)
 admin.site.register(models.Event)
-admin.site.register(models.Participation)
+admin.site.register(models.Participation, ParticipationAdmin)
