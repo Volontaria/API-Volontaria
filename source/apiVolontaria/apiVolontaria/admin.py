ad581b63355c5f24850ea426d9285e1858a6b797
RignonNoel <rignon.noel@gmail.com>
2018-01-27 16:10:33 -0500

    add is_active column on user admin panel

--- source/apiVolontaria/apiVolontaria/admin.py
+++ source/apiVolontaria/apiVolontaria/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
 from . import models
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    ]
+
+    list_filter = [
+        'is_staff',
+        'is_superuser',
+        'is_active',
+    ]
+
 
 admin.site.register(models.TemporaryToken)
 admin.site.register(models.Profile)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
