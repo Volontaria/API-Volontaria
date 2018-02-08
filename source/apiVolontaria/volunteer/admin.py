5387b9cf8e8625ac8e99c89e0f992c2170d1cb68
Yanic Olivier <yanic.olivier@gmail.com>
2018-02-08 15:10:19 -0500

    Add Filtering to Participation Admin

--- source/apiVolontaria/volunteer/admin.py
+++ source/apiVolontaria/volunteer/admin.py
@@ -7,6 +7,13 @@ class ParticipationAdmin(admin.ModelAdmin):
     search_fields = ['user__username']
     list_display = ['user', 'standby', 'start_date', 'end_date', 'cell']
 
+    list_filter = [
+        'event__start_date',
+        'event__cycle__name',
+        'event__task_type',
+    ]
+    date_hierarchy = 'event__start_date'
+
 
 admin.site.register(models.Cycle)
 admin.site.register(models.TaskType)
