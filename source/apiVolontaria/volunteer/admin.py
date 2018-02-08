3513ae66f8a86576781bd2ec3147de5b47e7c4a1
RignonNoel <rignon.noel@gmail.com>
2018-02-08 10:38:44 -0500

    Change to the Participations admin

--- source/apiVolontaria/volunteer/admin.py
+++ source/apiVolontaria/volunteer/admin.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
 
 from . import models
+from apiVolontaria.models import Profile
 
 
 class ParticipationAdmin(admin.ModelAdmin):
-    search_fields = ['user__username']
-    list_display = ['user', 'standby', 'start_date', 'end_date', 'cell']
+    search_fields = [
+        'user__username',
+        'user__first_name',
+        'user__last_name',
+        'user__email',
+    ]
+
+    list_display = [
+        'standby',
+        'user__first_name',
+        'user__last_name',
+        'user__email',
+        'user__phone',
+        'user__mobile',
+        'start_date',
+        'end_date',
+        'cell'
+    ]
+
+    ordering = ('event__start_date',)
 
     list_filter = [
         'event__start_date',
@@ -13,6 +32,35 @@ class ParticipationAdmin(admin.ModelAdmin):
         'event__task_type',
     ]
     date_hierarchy = 'event__start_date'
+
+    list_filter = [
+        'event__start_date',
+        'event__cycle__name',
+        'event__task_type',
+    ]
+    date_hierarchy = 'event__start_date'
+
+    @staticmethod
+    def user__email(obj):
+        return obj.user.email
+
+    @staticmethod
+    def user__first_name(obj):
+        return obj.user.first_name
+
+    @staticmethod
+    def user__last_name(obj):
+        return obj.user.last_name
+
+    @staticmethod
+    def user__phone(obj):
+        profile = Profile.objects.filter(user__pk=obj.user.pk).first()
+        return profile.phone if profile else ''
+
+    @staticmethod
+    def user__mobile(obj):
+        profile = Profile.objects.filter(user__pk=obj.user.pk).first()
+        return profile.mobile if profile else ''
 
 
 admin.site.register(models.Cycle)
