03fd60538cf68590930242ebce13cb292c24fc98
RignonNoel <rignon.noel@gmail.com>
2018-03-16 14:34:21 -0400

    refactor endpoints to have new managers field

--- source/apiVolontaria/volunteer/serializers.py
+++ source/apiVolontaria/volunteer/serializers.py
@@ -4,6 +4,10 @@ from django.db import IntegrityError
 
 from location.serializers import AddressBasicSerializer
 from location.models import Address, StateProvince, Country
+
+from apiVolontaria.serializers import UserPublicSerializer
+from django.contrib.auth.models import User
+
 from . import models
 
 
@@ -40,10 +44,15 @@ class CellBasicSerializer(serializers.ModelSerializer):
     """This class represents the Cell model serializer."""
 
     address = serializers.DictField()
+    managers = serializers.ListField(required=False)
 
     def create(self, validated_data):
         cell = models.Cell()
         cell.name = validated_data['name']
+        if 'managers' in validated_data.keys():
+            managers = validated_data['managers']
+        else:
+            managers = None
         address_data = validated_data['address']
         country_data = validated_data['address']['country']
         state_province_data = validated_data['address']['state_province']
@@ -98,65 +107,114 @@ class CellBasicSerializer(serializers.ModelSerializer):
                     raise serializers.ValidationError(error)
 
         cell.address = cell_address
+
+        list_manager = list()
+        if managers:
+            for manager in managers:
+                try:
+                    user = User.objects.get(id=manager)
+                    list_manager.append(user)
+                except Exception as err:
+                    error = {
+                        'message': (
+                            "Unknown user with this ID"
+                            if err.args else "Unknown Error"
+                        )
+                    }
+                    raise serializers.ValidationError(error)
+
         cell.save()
+        for user in list_manager:
+            cell.managers.add(user)
         return cell
 
     def update(self, instance, validated_data):
-        instance.name = validated_data['name']
-        address_data = validated_data['address']
-        country_data = validated_data['address']['country']
-        state_province_data = validated_data['address']['state_province']
+        if 'name' in validated_data.keys():
+            instance.name = validated_data['name']
+        if 'managers' in validated_data.keys():
+            # Remove all managers of the cell
+            instance.managers.clear()
 
-        address_data['country'] = country_data['iso_code']
-        address_data['state_province'] = state_province_data['iso_code']
-
-        try:
-            cell_address = Address.objects.get(**address_data)
-        except Address.DoesNotExist:
-            try:
-                cell_address = Address()
-                cell_address.__dict__.update(validated_data['address'])
-                cell_address.country, created = Country.objects.get_or_create(
-                    name__iexact=country_data['name'],
-                    iso_code__iexact=country_data['iso_code'],
-                    defaults={
-                        'iso_code': country_data['iso_code'],
-                        'name': country_data['name'],
-                    },
-                )
-            except IntegrityError as err:
-                if 'UNIQUE constraint failed' in err.args[0]:
+            for manager in validated_data['managers']:
+                try:
+                    user = User.objects.get(id=manager)
+                    instance.managers.add(user)
+                except Exception as err:
                     error = {
                         'message': (
-                            "A Country with that iso_code already exists"
+                            "Unknown user with this ID"
                             if err.args else "Unknown Error"
                         )
                     }
                     raise serializers.ValidationError(error)
+        if 'address' in validated_data.keys():
             try:
-                cell_address.state_province, created = (
-                    StateProvince.objects.get_or_create(
-                        iso_code__iexact=state_province_data['iso_code'],
-                        name__iexact=state_province_data['name'],
-                        country=cell_address.country,
-                        defaults={
-                            'iso_code': state_province_data['iso_code'],
-                            'name': state_province_data['name'],
-                        },
+                address_data = validated_data['address']
+                country_data = validated_data['address']['country']
+                state_province_data = \
+                    validated_data['address']['state_province']
+
+                address_data['country'] = country_data['iso_code']
+                address_data['state_province'] = \
+                    state_province_data['iso_code']
+            except KeyError as err:
+                error = {
+                    'message': (
+                        "Please specify a complete valid address."
+                        if err.args else "Unknown Error"
                     )
-                )
-                cell_address.save()
-            except IntegrityError as err:
-                if 'UNIQUE constraint failed' in err.args[0]:
-                    error = {
-                        'message': (
-                            "A StateProvince with that iso_code already exists"
-                            if err.args else "Unknown Error"
-                        )
-                    }
-                    raise serializers.ValidationError(error)
+                }
+                raise serializers.ValidationError(error)
 
-        instance.address = cell_address
+            try:
+                cell_address = Address.objects.get(**address_data)
+            except Address.DoesNotExist:
+                try:
+                    cell_address = Address()
+                    cell_address.__dict__.update(validated_data['address'])
+                    cell_address.country, created = \
+                        Country.objects.get_or_create(
+                            name__iexact=country_data['name'],
+                            iso_code__iexact=country_data['iso_code'],
+                            defaults={
+                                'iso_code': country_data['iso_code'],
+                                'name': country_data['name'],
+                            },
+                        )
+                except IntegrityError as err:
+                    if 'UNIQUE constraint failed' in err.args[0]:
+                        error = {
+                            'message': (
+                                "A Country with that iso_code already exists"
+                                if err.args else "Unknown Error"
+                            )
+                        }
+                        raise serializers.ValidationError(error)
+                try:
+                    cell_address.state_province, created = (
+                        StateProvince.objects.get_or_create(
+                            iso_code__iexact=state_province_data['iso_code'],
+                            name__iexact=state_province_data['name'],
+                            country=cell_address.country,
+                            defaults={
+                                'iso_code': state_province_data['iso_code'],
+                                'name': state_province_data['name'],
+                            },
+                        )
+                    )
+                    cell_address.save()
+                except IntegrityError as err:
+                    if 'UNIQUE constraint failed' in err.args[0]:
+                        error = {
+                            'message': (
+                                "A StateProvince with "
+                                "that iso_code already exists"
+                                if err.args else "Unknown Error"
+                            )
+                        }
+                        raise serializers.ValidationError(error)
+
+            instance.address = cell_address
 
         instance.save()
 
@@ -169,6 +227,13 @@ class CellBasicSerializer(serializers.ModelSerializer):
         data['address'] = AddressBasicSerializer(
             instance.address
         ).to_representation(instance.address)
+        data['managers'] = list()
+        for manager in instance.managers.all():
+            data['managers'].append(
+                UserPublicSerializer(
+                    manager
+                ).to_representation(manager)
+            )
         return data
 
     class Meta:
@@ -178,6 +243,7 @@ class CellBasicSerializer(serializers.ModelSerializer):
             'id',
             'name',
             'address',
+            'managers'
         )
         read_only_fields = [
             'id',
