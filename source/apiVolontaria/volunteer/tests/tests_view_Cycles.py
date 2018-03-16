dab8ae19e2bdb6c48825efea10f4bde76e835f16
RignonNoel <rignon.noel@gmail.com>
2018-03-16 11:18:31 -0400

    add test on is_active param

--- source/apiVolontaria/volunteer/tests/tests_view_Cycles.py
+++ source/apiVolontaria/volunteer/tests/tests_view_Cycles.py
@@ -29,6 +29,14 @@ class CyclesTests(APITestCase):
             ),
         )
 
+        self.past_cycle = Cycle.objects.create(
+            name='Cycle 2',
+            start_date=timezone.now() - timezone.timedelta(
+                days=10,
+            ),
+            end_date=timezone.now(),
+        )
+
     def test_create_new_cycle_with_permission(self):
         """
         Ensure we can create a new cycle if we have the permission.
@@ -83,6 +91,25 @@ class CyclesTests(APITestCase):
         start_date = self.cycle.start_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
         end_date = self.cycle.end_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
+        data = {
+            "id": self.cycle.id,
+            "name": self.cycle.name,
+            "start_date": start_date,
+            "end_date": end_date,
+        }
+
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.get(reverse('volunteer:cycles'))
+
+        self.assertEqual(json.loads(response.content)['results'][0], data)
+        self.assertEqual(json.loads(response.content)['count'], 2)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_list_cycle_when_is_active_true(self):
+        start_date = self.cycle.start_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        end_date = self.cycle.end_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
         data = [
             {
                 "id": self.cycle.id,
@@ -94,8 +121,63 @@ class CyclesTests(APITestCase):
 
         self.client.force_authenticate(user=self.user)
 
-        response = self.client.get(reverse('volunteer:cycles'))
+        url = "{0}?is_active=True".format(
+            reverse('volunteer:cycles')
+        )
+
+        response = self.client.get(url)
 
         self.assertEqual(json.loads(response.content)['results'], data)
         self.assertEqual(json.loads(response.content)['count'], 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_list_cycle_when_is_active_false(self):
+        start_date = self.past_cycle.start_date\
+            .strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        end_date = self.past_cycle.end_date\
+            .strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+        data = [
+            {
+                "id": self.past_cycle.id,
+                "name": self.past_cycle.name,
+                "start_date": start_date,
+                "end_date": end_date,
+            }
+        ]
+
+        self.client.force_authenticate(user=self.user)
+
+        url = "{0}?is_active=False".format(
+            reverse('volunteer:cycles')
+        )
+
+        response = self.client.get(url)
+
+        self.assertEqual(json.loads(response.content)['count'], 1)
+        self.assertEqual(json.loads(response.content)['results'], data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_list_cycle_when_is_active_bad_input(self):
+        start_date = self.cycle.start_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        end_date = self.cycle.end_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+        data = {
+            "id": self.cycle.id,
+            "name": self.cycle.name,
+            "start_date": start_date,
+            "end_date": end_date,
+        }
+
+        self.client.force_authenticate(user=self.user)
+
+        # 'bad' is not a good input
+        url = "{0}?is_active=bad".format(
+            reverse('volunteer:cycles')
+        )
+
+        response = self.client.get(url)
+
+        self.assertEqual(json.loads(response.content)['results'][0], data)
+        self.assertEqual(json.loads(response.content)['count'], 2)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
