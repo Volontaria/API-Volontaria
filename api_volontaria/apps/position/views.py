b8c1188cbee662f8493fd69c0d885103db900e0c
vardane <yjjpmfoucault@yahoo.ca>
2020-09-22 22:17:23 -0400

    Addressed some of reviewer's comments. Non final.

--- api_volontaria/apps/position/views.py
+++ api_volontaria/apps/position/views.py
@@ -21,15 +21,7 @@ class PositionViewSet(viewsets.ModelViewSet):
     queryset = Position.objects.all()
     filter_fields = '__all__'
     permission_classes = (DRYPermissions,)
-
-    def get_permissions(self):
-        if self.action in ['list', 'retrieve']:
-            permission_classes = []
-        else:
-            permission_classes = [IsAdminUser]
-
-        return [permission() for permission in permission_classes]
-
+    
 
 class ApplicationViewSet(viewsets.ModelViewSet):
 
@@ -37,40 +29,3 @@ class ApplicationViewSet(viewsets.ModelViewSet):
     queryset = Application.objects.all()
     filter_fields = '__all__'
     permission_classes = (DRYPermissions,)
-
-    # def get_permissions(self):
-    #     if self.action in ['list']:
-    #         permission_classes = []
-    #     else:
-    #         permission_classes = [IsAdminUser]
-
-    #     return [permission() for permission in permission_classes]
-
-
-#TODO: determine whether classes below are required for Position or Application
-
-# class ParticipationFilterBackend(DRYPermissionFiltersBase):
-
-#     def filter_list_queryset(self, request, queryset, view):
-#         """
-#         Limits all list requests to only be seen by the staff or owner.
-#         """
-#         if request.user.is_staff:
-#             return queryset
-#         else:
-#             return queryset.filter(user=request.user)
-
-
-# class ParticipationViewSet(viewsets.ModelViewSet):
-
-#     serializer_class = ParticipationSerializer
-#     queryset = Participation.objects.all()
-#     filterset_fields = {
-#         'registered_at': ['exact', 'gte', 'lte'],
-#         'event__start_time': ['exact', 'gte', 'lte'],
-#         'event__end_time': ['exact', 'gte', 'lte'],
-#         'presence_status': ['exact'],
-#         'is_standby': ['exact']
-#     }
-#     permission_classes = (DRYPermissions,)
-#     filter_backends = (ParticipationFilterBackend, DjangoFilterBackend)
