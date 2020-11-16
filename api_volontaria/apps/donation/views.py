6d19f85b0ada4c234fbfd239218be2a24779f8c5
imanethepe <toquenathalie@gmail.com>
2020-11-16 18:09:14 -0500

    'projet_donation'

--- api_volontaria/apps/donation/views.py
+++ api_volontaria/apps/donation/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 from django_filters.rest_framework import DjangoFilterBackend
 from dry_rest_permissions.generics import DRYPermissions, \
     DRYPermissionFiltersBase
@@ -21,18 +22,18 @@ class BankConnectionViewSet(viewsets.ModelViewSet):
     queryset = BankConnection.objects.all()
     serializer_class = BankConnectionSerializer
     filter_fields = '__all__'
-    permission_classes = (DRYPermissions,)
+##    permission_classes = (DRYPermissions,)
     
 class StripeConnectionViewSet(viewsets.ModelViewSet):
 
     queryset = StripeConnection.objects.all()
     serializer_class = StripeConnectionSerializer
     filter_fields = '__all__'
-    permission_classes = (DRYPermissions,)
+##    permission_classes = (DRYPermissions,)
     
 class DonationViewSet(viewsets.ModelViewSet):
 
     queryset = Donation.objects.all()
     serializer_class = DonationSerializer
     filter_fields = '__all__'
-    permission_classes = (DRYPermissions,)
+##    permission_classes = (DRYPermissions,)
