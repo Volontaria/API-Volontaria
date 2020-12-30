b7e7778ca756233daac116135c9ae5a9d08126ef
Yanic <yanic.olivier@gmail.com>
2020-12-30 11:20:33 -0500

    Replaced the password form in Django admin of User (#273)

--- api_volontaria/apps/user/admin.py
+++ api_volontaria/apps/user/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
 
 from api_volontaria.apps.user.models import User, ActionToken
 
-admin.site.register(User)
+
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser',
+                'groups', 'user_permissions'
+            ),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('email',)
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(ActionToken)
